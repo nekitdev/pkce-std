e54d4ffe9ff05e8a150d55748ef064771b4a8f10
Nikita Tikhonov <nekitdevofficial@gmail.com>
2025-01-27 15:36:32 +0300

    Prepare for the stable release.

--- README.md
+++ README.md
@@ -1,3 +1,92 @@
 # `pkce-std`
 
-> *Handling Proof Key for Code Exchange values.*
+[![License][License Badge]][License]
+[![Version][Version Badge]][Crate]
+[![Downloads][Downloads Badge]][Crate]
+[![Test][Test Badge]][Actions]
+
+> *Handling Proof Key for Code Exchange.*
+
+## Installation
+
+### `cargo`
+
+You can add `pkce-std` as a dependency with the following command:
+
+```console
+$ cargo add pkce-std
+```
+
+Or by directly specifying it in the configuration like so:
+
+```toml
+[dependencies]
+pkce-std = "0.1.0"
+```
+
+Alternatively, you can add it directly from the source:
+
+```toml
+[dependencies.pkce-std]
+git = "https://github.com/nekitdev/pkce-std.git"
+```
+
+## Example
+
+```rust
+use pkce_std::Code;
+
+fn main() {
+    let code = Code::generate_default();
+
+    let (verifier, challenge) = code.into_pair();
+
+    assert!(verifier.verify(&challenge));
+}
+```
+
+## Documentation
+
+You can find the documentation [here][Documentation].
+
+## Support
+
+If you need support with the library, you can send an [email][Email].
+
+## Changelog
+
+You can find the changelog [here][Changelog].
+
+## Security Policy
+
+You can find the Security Policy of `pkce-std` [here][Security].
+
+## Contributing
+
+If you are interested in contributing to `pkce-std`, make sure to take a look at the
+[Contributing Guide][Contributing Guide], as well as the [Code of Conduct][Code of Conduct].
+
+## License
+
+`pkce-std` is licensed under the MIT License terms. See [License][License] for details.
+
+[Email]: mailto:support@nekit.dev
+
+[Discord]: https://nekit.dev/chat
+
+[Actions]: https://github.com/nekitdev/pkce-std/actions
+
+[Changelog]: https://github.com/nekitdev/pkce-std/blob/main/CHANGELOG.md
+[Code of Conduct]: https://github.com/nekitdev/pkce-std/blob/main/CODE_OF_CONDUCT.md
+[Contributing Guide]: https://github.com/nekitdev/pkce-std/blob/main/CONTRIBUTING.md
+[Security]: https://github.com/nekitdev/pkce-std/blob/main/SECURITY.md
+
+[License]: https://github.com/nekitdev/pkce-std/blob/main/LICENSE
+
+[Crate]: https://crates.io/crates/pkce-std
+[Documentation]: https://docs.rs/pkce-std
+
+[License Badge]: https://img.shields.io/crates/l/pkce-std
+[Version Badge]: https://img.shields.io/crates/v/pkce-std
+[Downloads Badge]: https://img.shields.io/crates/dr/pkce-std
+[Test Badge]: https://github.com/nekitdev/pkce-std/workflows/test/badge.svg
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,100 @@
-//! Handling Proof Key for Code Exchange values.
+//! Handling Proof Key for Code Exchange.
 //!
 //! PKCE specification is defined in [RFC 7636](https://datatracker.ietf.org/doc/html/rfc7636).
+//!
+//! # Abstract
+//!
+//! PKCE (pronounced "pixy") is a method to secure authorization codes in OAuth 2.0
+//! *authorization code* flow. It is designed to prevent interception attacks.
+//!
+//! # Abbreviations
+//!
+//! - `PKCE`: Proof Key for Code Exchange;
+//! - `Auth`: Authorization.
+//!
+//! # Protocol
+//!
+//! ```text
+//!                                                   +--------------+
+//!                                                   |     Auth     |
+//! +--------+                                        | +----------+ |
+//! |        |--(1)- Auth Request + code_challenge ---->|          | |
+//! |        |         + code_challenge_method        | |   Auth   | |
+//! |        |                                        | | Endpoint | |
+//! |        |<-(2)----------- Auth Code ---------------|          | |
+//! |        |                                        | +----------+ |
+//! | Client |                                        |              |
+//! |        |                                        | +----------+ |
+//! |        |--(3)- Token Request + code_verifier ---->|          | |
+//! |        |                                        | |  Tokens  | |
+//! |        |                                        | | Endpoint | |
+//! |        |<-(4)------------- Token -----------------|          | |
+//! +--------+                                        | +----------+ |
+//!                                                   |    Server    |
+//!                                                   +--------------+
+//! ```
+//!
+//! ## 0. Code creation
+//!
+//! The *client* generates the `code_verifier` and derives the `code_challenge` from it
+//! using the `code_challenge_method`.
+//!
+//! ## 1. Auth Request
+//!
+//! The *client* sends the `code_challenge` and the `code_challenge_method` along with the
+//! regular `authorization_code` request to the *Auth Endpoint*.
+//!
+//! ## 2. Auth Code
+//!
+//! The *server* stores the `code_challenge` and the `code_challenge_method` for later use,
+//! responding with the usual authorization `code`.
+//!
+//! ## 3. Token Request
+//!
+//! The *client* sends the `code_verifier` along with the regular request to the *Tokens Endpoint*.
+//!
+//! ## 4. Token
+//!
+//! The *server* verifies the `code_verifier` against the stored `code_challenge` using the
+//! `code_challenge_method`, responding with the `token` if the verification is successful.
+//!
+//! # Examples
+//!
+//! Generating `code_verifier` and deriving `code_challenge` in one go:
+//!
+//! ```
+//! use pkce_std::Code;
+//!
+//! let code = Code::generate_default();
+//! ```
+//!
+//! Alternatively, generating from random bytes:
+//!
+//! ```
+//! use pkce_std::Code;
+//!
+//! let code = Code::generate_encode_default();
+//! ```
+//!
+//! Decoupling verifier and challenge:
+//!
+//! ```
+//! # use pkce_std::Code;
+//! #
+//! # let code = Code::generate_default();
+//! #
+//! let (verifier, challenge) = code.into_pair();
+//! ```
+//!
+//! Verifying `code_verifier` against `code_challenge`:
+//!
+//! ```
+//! # use pkce_std::Code;
+//! #
+//! # let (verifier, challenge) = Code::generate_default().into_pair();
+//! #
+//! let valid = verifier.verify(&challenge);
+//! ```
 
 #![warn(missing_docs)]
 
